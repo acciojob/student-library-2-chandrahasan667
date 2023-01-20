9296329fdbbec23233e569310aec5a42834a9e71
chandrahasan <chandrahasan667@gmail.com>
2023-01-20 13:25:36 +0530

    code

--- src/main/java/com/driver/models/CardStatus.java
+++ src/main/java/com/driver/models/CardStatus.java
@@ -1,5 +1,8 @@
 package com.driver.models;
 
+
+import javax.persistence.Table;
+@Table(name = "CardStatus")
 public enum CardStatus {
     ACTIVATED,
     DEACTIVATED
--- src/main/java/com/driver/services/BookService.java
+++ src/main/java/com/driver/services/BookService.java
@@ -1,6 +1,7 @@
 package com.driver.services;
 
 import com.driver.models.Book;
+import com.driver.repositories.AuthorRepository;
 import com.driver.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,26 @@ public class BookService {
     @Autowired
     BookRepository bookRepository2;
 
+    @Autowired
+    AuthorRepository authorRepository;
+
     public void createBook(Book book){
         bookRepository2.save(book);
     }
 
     public List<Book> getBooks(String genre, boolean available, String author){
-        List<Book> books = null; //find the elements of the list by yourself
-        return books;
+        if(genre != null && author != null){
+            return bookRepository2.findBooksByGenreAuthor(genre, author, available);
+        }
+        else if(genre != null){
+            return bookRepository2.findBooksByGenre(genre, available);
+        }
+        else if(author != null){
+            return bookRepository2.findBooksByAuthor(author, available);
+        }
+        else{
+            return bookRepository2.findByAvailability(available);
+        }
     }
+
 }
